8042711345afb26987150ebb993a1d17b36eba7d
Yun-Ching Liu <cipherman@gmail.com>
2025-12-31 18:51:46 +0900

    simple chat app with gemma3:27b-cloud using ollama

--- app.py
+++ app.py
@@ -1,7 +1,49 @@
+import os
+from dotenv import load_dotenv
+
 import streamlit as st
-import numpy as np
+from openai import OpenAI
 
 
-with st.chat_message("assistant"):
-    st.write("Hello human")
-    st.bar_chart(np.random.randn(30, 3))
+st.title("Simple Chat App with Ollama")
+
+load_dotenv()
+OLLAMA_API_KEY = os.getenv("OLLAMA_API_KEY")
+
+client = OpenAI(
+    base_url="https://ollama.com/v1",
+    api_key=OLLAMA_API_KEY,
+)
+
+# Set default model if not already set
+if "chat_llm" not in st.session_state:
+    st.session_state["chat_llm"] = "gemma3:27b-cloud"
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content": "You are a helpful assistant."}
+    ]
+
+# Display chat messages from history on app rerun
+for message in st.session_state["messages"]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["chat_llm"],
+            messages=[
+                {"role": msg["role"], "content": msg["content"]}
+                for msg in st.session_state["messages"]
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
